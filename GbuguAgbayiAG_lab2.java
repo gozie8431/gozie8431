29dcacc3d8ee1f71ad393b9ee5e21d4a1bfa4dd2
gozie8431 <55507490+gozie8431@users.noreply.github.com>
2019-09-18 13:46:36 -0400

    class code

---
+++ GbuguAgbayiAG_lab2.java
@@ -0,0 +1,35 @@
+public class GbuguAgbayiAG_lab2
+
+//Agbayi Gbugu, 00454992, computer engineering, 09-18-2019 12:18.
+
+{
+   public static void main(String[] args)
+  
+   // Part A.(two intergers were defined "distance and time"). 
+   { int distance = 305;
+      int time = 8;
+    System.out.println(" Part A: " + "\"" + "Variable and Literals" + "\"");
+    System.out.println("the distance between two cities is " + distance + " miles and it takes " + time + " hours to drive between them.");
+    
+   
+    
+   // Part B.(three where defined "Total, Cost, and Tax" and cost and tax where calcluated to find the total).
+   double Total, Cost, and, Tax;
+      Total = 349.6;
+      Cost = 320.0;
+      Tax = 0.925;
+   System.out.println(" Part B: " + "\"" + "Arithmetic Operators 1" + "\"");
+    System.out.println("cost is " + Cost + " the sales tax is " + Tax + " and the total is " + Total + " . " );
+    
+    
+       // Part c.(four intergers were defined "dividend, divisor, quotient, And, remainder" and the quotient was calculated dividing by dividend by the divisor).
+   int dividend, divisor, quotient, And, remainder;
+    dividend = 23;
+    divisor = 7;
+    quotient =3;
+    remainder =2;
+   System.out.println(" Part C: " + "\"" + "Arithmetic Operators 2" + "\"");
+    System.out.println("when " +  dividend + " is dividend by " + divisor + " and the quotient is " + quotient + " and the remainder is " + remainder +" . " );
+    }
+}    
+
