d52d5a172191c031d6873f8a4ef33663686b152f
gozie8431 <55507490+gozie8431@users.noreply.github.com>
2019-09-21 16:25:59 -0400

    Add files via upload

---
+++ GbuguA_P1.java
@@ -0,0 +1,50 @@
+/* Name: Agbayi Gbugu
+   Assignment#: 1
+   Due date: 9/30/2019
+   Location: ST 126 
+*/
+
+public class GbuguA_P1
+{
+   public static void main(string[]args);
+   
+  { 
+   String name;
+   double a, b, c;
+    a= 7.0;
+    b= 9.0;
+    c= 6.0;
+    result1 = 279957;
+    result2 = -7803;
+    result3 = 6.875;
+    result4 = 9.83;
+     279957 = c^7 + 3a;
+     -7803 = b^3 - 3a^3b + b^3;
+     6.875 = (3a-2/4) + (2b-1/8);
+     9.83 = (4c^3+2/3);
+   }
+ { 
+   Scanner in=new Scanner(System.in);
+   System.out.println("(c^7 + 3a)");
+   "result1"=in.next();
+   System.out.println("you answer is" + result1 + ".");
+ }
+{
+   Scanner in=new Scanner(System.in);
+   System.out.println("(b^3 - 3a3b + b^3)");
+   "result2"=in.next();
+   System.out.println("you answer is" + result2 + ".");
+ }
+  { 
+   Scanner in=new Scanner(System.in);
+   System.out.println("((3a-2/4) + (2b-1/8))");
+   "result3"=in.next();
+   System.out.println("you answer is" + result3 + ".");
+  }
+  {
+   Scanner in=new Scanner(System.in);
+   System.out.println("((4c^3+2/3))");
+   "result4"=in.next();
+   System.out.println("you answer is" + result4 + ".");
+  }
+}
