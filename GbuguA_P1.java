ab0a3dc1d5ab2b4e64f255f8711f8069fa153ddb
gozie8431 <55507490+gozie8431@users.noreply.github.com>
2019-09-30 22:50:01 -0400

    Add files via upload

--- GbuguA_P1.java
+++ GbuguA_P1.java
@@ -11,16 +11,21 @@ public class GbuguA_P1
    
   { 
    String name;
+   
+   /*B*/ //All variables must be converted to first initial and last initial
    double aAG, bAG, rAG;
     aAG= 7.0;
     bAG= 9.0;
     rAG= 6.0;
+   
+   /*C*/
     double c=5.0;
       double result1 = Math.pow(c,7) + 3*aAG;
      double result2 = Math.pow(bAG,3) - Math.pow(3*aAG,3)*bAG + Math.pow(bAG,3);
     double result3 = (3*aAG-2/4) + (2*bAG-1/8);
      double result4 = Math.pow(4*rAG,3)+ 2/3;
    
+   /*D*/
  
    Scanner keybored=new Scanner(System.in);
    System.out.println("result1="+ result1);
@@ -31,6 +36,7 @@ public class GbuguA_P1
    
    System.out.println("result4="+ result4);
    
+   /*E*/
    Scanner in=new Scanner(System.in);
    System.out.println("Enter values,(make sure they are doubles)");
    
@@ -42,7 +48,6 @@ public class GbuguA_P1
      double newresult2 = Math.pow(bAG,3) - Math.pow(3*aAG,3)*bAG + Math.pow(bAG,3);
     double newresult3 = (3*aAG-2/4) + (2*bAG-1/8);
      double newresult4 = Math.pow(4*rAG,3)+ 2/3;
-   
 
    
    System.out.println("newresult1="+ newresult1);
@@ -55,24 +60,28 @@ public class GbuguA_P1
    
    System.out.println("input values for strings");
    
-   String string1=keybored.nextLine();
-   
-     keybored.nextLine();
+    keybored.nextLine();
+  
+  /*F*/ //Define three String data types
+  
+   String string1=keybored.nextLine();/*enter value up to 4 letters*/ 
    
    String string2=keybored.nextLine();
-    
-       
+ 
+   String string3 = (string1 +" "+ string2);
+
+   /*G*/  
    int Size;
 
-    
+    /*H*/ //Determine the length of each String
      Scanner length=new Scanner(System.in);
 
    System.out.print(" string1 contains how many characters?" + "\n");
    String size1=keybored.nextLine();
 
 
-  System.out.print(" string1" +" "+ size1+ "\n");
-   
+  System.out.print(" string1 =" +" "+ size1+ "\n");
+    System.out.print("\n");
    
      Scanner length2=new Scanner(System.in);
 
@@ -80,13 +89,35 @@ public class GbuguA_P1
    String size2=keybored.nextLine();
 
 
-  System.out.print(" string2=" +" "+ size2+ "\n");
+   System.out.print(" string2 =" +" "+ size2+ "\n");
+   System.out.print("\n");
+    /* String string3 = (string1 +" "+ string2);*/ 
+  
+  /*I*/
+   System.out.println("string3 =" +" "+ string3);
+  System.out.print("\n");
+  /*J*/ //Convert String3 to all upper case
+   System.out.println("string3 toUpperCase =" +" "+ string3.toUpperCase());
+   System.out.print("\n");
+   /*K*/ //use a string method not defined in the textbook and print out your results
+  System.out.println(string1.substring(0,3));//new string method\\
+   System.out.print("\n");
+  /*L*/ //Define casting in comments.
+   System.out.println("input interger");
    
-   System.out.println((string1)+""+(string2));
-   String string3=keybored.nextLine();
- 
+   double x;
+   
+   int y=4;
 
+   x = (double)y;
    
+   System.out.println("the double is" +" " + x +","+" "+ "the int is" +" "+ y);
+   System.out.print("\n");
+   /*M*/ //demonstrate the use of a named constant
+   
+   final double score = 2;
+    
+    System.out.println(Math.pow(score,7) + 3*score);
    
    }
-} 
+}
